851167850d2ba058dfe6f902b45f6c3e27d35510
Jerakin <the_rensvind@hotmail.com>
2025-10-02 12:55:22 +0200

    fixes to the api

--- logging.lua
+++ logging.lua
@@ -1,13 +1,14 @@
 ---@class record
 ---@field name string
----@field level number
+---@field level number One of the logging levels
 ---@field pathname string
 ---@field lineno number
 ---@field msg string
 
 ---@class handler
----@field handle fun(record: record): nil
 ---@field name string
+---@field level number One of the logging levels
+---@field handle fun(record: record): nil
 ---@field formatter? fun(record: record): string
 
 ---@class logger
@@ -16,6 +17,8 @@
 ---@field warning fun(...): nil
 ---@field error fun(...): nil
 ---@field critical fun(...): nil
+---@field name string Name this logger was constructed with
+---@field parent string Name of the parent logger
 ---@field propagate boolean If this logger should propegate to parent loggers
 ---@field set_level fun(name_or_level: number|string): nil
 ---@field get_level fun(): number
@@ -23,6 +26,7 @@
 ---@field remove_handler fun(name: string): boolean
 
 ---@class logging_module
+---@field NOTSET number
 ---@field DEBUG number
 ---@field INFO number
 ---@field WARNING number
@@ -34,20 +38,21 @@
 ---@field error fun(...): nil
 ---@field critical fun(...): nil
 ---@field get_logger fun(name?: string): logger
----@field handlers table<string, handler> Predefined handlers 
-
+---@field handlers table<string, handler> Predefined handlers
+---@field set_default_formatter fun(formatter: fun(record:record):string)
+---@field get_default_formatter fun(): fun(record:record): string
+---@field get_level_name fun(level: number): string
 
 ---@type logging_module
 local logging = {
-	__HOMEPAGE = 'https://github.com/Jerakin/logging.lua',
-	__DESCRIPTION = "Complex enough logging library." ,
-	__VERSION = '0.1.0',
+	__HOMEPAGE = "https://github.com/Jerakin/logging.lua",
+	__DESCRIPTION = "Complex enough logging library.",
+	__VERSION = "0.1.0",
 }
 
-logging.handlers = {
+logging.handlers = {}
 
-}
-
+logging.NOTSET = 0
 logging.DEBUG = 10
 logging.INFO = 20
 logging.WARNING = 30
@@ -83,12 +88,11 @@ local __string_split = function(inputstr, sep)
 		sep = "%s"
 	end
 	local t = {}
-	for str in __lua_string_gmatch(inputstr, "([^"..sep.."]+)") do
+	for str in __lua_string_gmatch(inputstr, "([^" .. sep .. "]+)") do
 		__lua_table_insert(t, str)
 	end
 	return t
 end
-
 
 local __round = function(x, increment)
 	increment = increment or 1
@@ -113,38 +117,55 @@ local function __set_logging_level(t, name_or_level)
 	if var_type == "string" then
 		local new_level = __levels[name_or_level]
 		assert(new_level ~= nil, "Not a valid logging level: " .. name_or_level)
-		t["_level"] = new_level
+		t.level = new_level
 	elseif var_type == "number" then
-		t["_level"] = name_or_level
+		t.level = name_or_level
 	else
 		assert(false, "Not a valid logging level: " .. name_or_level)
 	end
 end
-
 
 local function __default_formatter(record)
 	local lineinfo = record.pathname .. ":" .. record.lineno
 	return string.format("[%s] [%s] %s: %s", __level_to_name[record.level]:upper(), record.name, lineinfo, record.msg)
 end
 
+local function __record_factory(name, level, message)
+	local info = debug.getinfo(3, "Sl")
+	local record = {
+		name = name,
+		level = level,
+		pathname = info.short_src,
+		lineno = info.currentline,
+		msg = message,
+	}
+	return record
+end
 
 ----------------------------- Handlers --------------------------------------
 
-
 logging.handlers.print_handler = {
 	name = "print_handler",
+	level = logging.NOTSET,
 	handle = function(self, record)
+		if self.level ~= logging.NOTSET and record.level < self.level then
+			return
+		end
+
 		local formatter = self.formatter or __default_formatter
 		print(formatter(record))
-	end
+	end,
 }
-
 
 -- Example file handler
 logging.handlers.file_handler = {
 	name = "file_handler",
 	file_path = nil,
+	level = logging.NOTSET,
 	handle = function(self, record)
+		if self.level ~= logging.NOTSET and record.level < self.level then
+			return
+		end
 		if self.file_path == nil then
 			return
 		end
@@ -156,10 +177,8 @@ logging.handlers.file_handler = {
 			fp:write(str)
 			fp:close()
 		end
-	end
+	end,
 }
-
-
 
 ------------------------------- Logger factory -------------------------------
 local __logger = {}
@@ -167,43 +186,30 @@ __logger.__index = __logger
 
 function __logger.new(name, parent)
 	local self = setmetatable({}, __logger)
-	self._level = logging.DEBUG
+	self.level = logging.NOTSET
 	self.propagate = true
-	self._name = name
+	self.name = name
 	self.parent = parent
-	self._handlers = {
-	}
+	self.handlers = {}
 	for log_name, log_level in pairs(__levels) do
 		self[log_name] = function(...)
-
 			local msg = __tostring(...)
-			local record = self:_make_record(msg, log_level)
+			local record = __record_factory(self.name, log_level, msg)
 			self:_emit(record)
 		end
 	end
 
 	return self
 end
-function __logger:_make_record(message, log_level)
-	local info = debug.getinfo(3, "Sl")
-	local record = {
-		name = self._name,
-		level = log_level,
-		pathname = info.short_src,
-		lineno = info.currentline,
-		msg=message
-	}
-	return record
-end
 
 function __logger:_emit(record)
-	if record.level < self._level then
+	if self.level ~= logging.NOTSET and record.level < self.level then
 		return
 	end
 	if self.parent and self.propagate then
 		__loggers[self.parent]:_emit(record)
 	end
-	for _, handler in ipairs(self._handlers) do
+	for _, handler in ipairs(self.handlers) do
 		handler:handle(record)
 	end
 end
@@ -212,20 +218,16 @@ function __logger:set_level(name_or_level)
 	__set_logging_level(self, name_or_level)
 end
 
-function __logger:get_level()
-	return self._level
-end
-
 function __logger:add_handler(handler)
 	assert(handler.name ~= nil, "Handler name required")
 	assert(handler.handle ~= nil, "Handler requires a handle function")
-	table.insert(self._handlers, handler)
+	table.insert(self.handlers, handler)
 end
 
 function __logger:remove_handler(name_)
-	for index, _handler in pairs(self._handlers) do
+	for index, _handler in pairs(self.handlers) do
 		if _handler.name == name_ then
-			table.remove(self._handlers, index)
+			table.remove(self.handlers, index)
 			return true
 		end
 	end
@@ -234,15 +236,13 @@ end
 
 -------------------------- Child to Parent Cache ----------------------------
 
-local __parent_child_relasionship = {
-
-}
+local __parent_child_relasionship = {}
 
 local function __update_child_parent_relasionship(name)
 	__parent_child_relasionship[name] = "root"
 	local name_hierarchy = __string_split(name, ".")
 
-	for _=#name_hierarchy, 1, -1 do
+	for _ = #name_hierarchy, 1, -1 do
 		table.remove(name_hierarchy, #name_hierarchy)
 		local parent = table.concat(name_hierarchy, ".")
 		if __parent_child_relasionship[parent] then
@@ -256,7 +256,7 @@ local function __update_child_parent_relasionship(name)
 	for child, _ in pairs(__parent_child_relasionship) do
 		local child_split = __string_split(child, ".")
 		for i = #child_split, 1, -1 do
-			local child_ =  table.concat({unpack(child_split, 1, i)}, ".")
+			local child_ = table.concat({ unpack(child_split, 1, i) }, ".")
 			if __parent_child_relasionship[child_] and child_ ~= child and child ~= name then
 				updated_relationships[child] = child_
 				__loggers[child].parent = child_
@@ -270,7 +270,6 @@ local function __update_child_parent_relasionship(name)
 		__loggers[k].parent = v
 	end
 end
-
 
 ------------------------------ Public Method --------------------------------
 
@@ -290,31 +289,38 @@ function logging.get_logger(name)
 	return __loggers[name]
 end
 
+function logging.set_default_formatter(formatter)
+	assert(type(formatter == "function"), "Formatter should be a function")
+	__default_formatter = formatter
+end
+
+function logging.get_default_formatter()
+	return __default_formatter
+end
+
+function logging.get_level_name(level)
+	return __level_to_name[level]
+end
 
 function logging.debug(...)
 	__root.debug(...)
 end
 
-
 function logging.info(...)
 	__root.info(...)
 end
-
 
 function logging.warning(...)
 	__root.critical(...)
 end
 
-
 function logging.error(...)
 	__root.critical(...)
 end
 
-
 function logging.critical(...)
 	__root.critical(...)
 end
-
 
 -- Setup root logger
 __root = __logger.new("root")
--- tests/tests_logger.lua
+++ tests/tests_logger.lua
@@ -1,17 +1,17 @@
 package.loaded.logging = nil
-local logging = require "logging"
-local lust = require "tests.lust"
+local logging = require("logging")
+local lust = require("tests.lust")
 
 lust.nocolor()
 
 local describe, it, expect = lust.describe, lust.it, lust.expect
 
 local test_handler = {
-    name = "test_handler",
-    record = nil,
-    handle = function(self, record)
-        self.record = record
-    end
+	name = "test_handler",
+	record = nil,
+	handle = function(self, record)
+		self.record = record
+	end,
 }
 
 local logger = logging.get_logger()
@@ -20,97 +20,94 @@ logger:remove_handler("print_handler")
 logger:add_handler(test_handler)
 
 describe("logging", function()
-    lust.before(function()
-        -- Probably need to clear the logger here.
-        logger:set_level(logging.DEBUG)
-        test_handler.record = nil
-    end)
+	lust.before(function()
+		-- Probably need to clear the logger here.
+		logger:set_level(logging.DEBUG)
+		test_handler.record = nil
+	end)
 
+	describe("basic-logging-level", function()
+		it("setting-logging-level", function()
+			logger:set_level(logging.ERROR)
+			expect(logger.level).to.equal(logging.ERROR)
+		end)
 
-    describe("basic-logging-level", function()
-        it("setting-logging-level", function()
-            logger:set_level(logging.ERROR)
-            expect(logger:get_level()).to.equal(logging.ERROR)
-        end)
+		it("setting-logging-level-number", function()
+			logger:set_level(15)
+			expect(logger.level).to.equal(15)
+		end)
 
-        it("setting-logging-level-number", function()
-            logger:set_level(15)
-            expect(logger:get_level()).to.equal(15)
-        end)
+		it("setting-logging-level-invalid", function()
+			local status, _ = xpcall(function()
+				logger:set_level("number")
+			end, function() end)
+			expect(status).to.equal(false)
+		end)
+	end)
 
-        it("setting-logging-level-invalid", function()
-            local status, _ = xpcall(function()
-                logger:set_level("number")
-            end, function() end)
-            expect(status).to.equal(false)
-        end)
-    end)
+	describe("basic-logging", function()
+		it("record-is-created", function()
+			local spy = lust.spy(test_handler, "handle")
+			local msg = "Doing some logging"
+			logger.debug(msg)
+			expect(test_handler.record.msg).to.equal(msg)
+			expect(#spy).to.equal(1)
+		end)
 
-    describe("basic-logging", function()
-        it("record-is-created", function()
-            local spy = lust.spy(test_handler, 'handle')
-            local msg = "Doing some logging"
-            logger.debug(msg)
-            expect(test_handler.record.msg).to.equal(msg)
-            expect(#spy).to.equal(1)
-        end)
+		it("record-is-ignored-with-level", function()
+			local spy = lust.spy(test_handler, "handle")
+			logger:set_level(logging.WARNING)
+			local msg = "Doing some logging"
+			logger.debug(msg)
+			expect(#spy).to.equal(0)
+		end)
+	end)
 
-        it("record-is-ignored-with-level", function()
-            local spy = lust.spy(test_handler, 'handle')
-            logger:set_level(logging.WARNING)
-            local msg = "Doing some logging"
-            logger.debug(msg)
-            expect(#spy).to.equal(0)
-        end)
-    end)
+	describe("hierarchial-logging", function()
+		it("root-handler-called", function()
+			local parent = logging.get_logger("test")
+			local msg = "Doing some logging"
+			local spy = lust.spy(test_handler, "handle")
+			parent.debug(msg)
+			expect(#spy).to.equal(1)
+		end)
 
-    describe("hierarchial-logging", function()
-        it("root-handler-called", function()
-            local parent = logging.get_logger("test")
-            local msg = "Doing some logging"
-            local spy = lust.spy(test_handler, 'handle')
-            parent.debug(msg)
-            expect(#spy).to.equal(1)
-        end)
+		it("propagation-off", function()
+			local parent = logging.get_logger("test")
+			parent.propagate = false
+			local msg = "Doing some logging"
+			local spy = lust.spy(test_handler, "handle")
+			parent.debug(msg)
+			expect(#spy).to.equal(0)
+		end)
 
-        it("propagation-off", function()
-            local parent = logging.get_logger("test")
-            parent.propagate = false
-            local msg = "Doing some logging"
-            local spy = lust.spy(test_handler, 'handle')
-            parent.debug(msg)
-            expect(#spy).to.equal(0)
-        end)
+		it("child-parent-relationship", function()
+			logging.get_logger("foo.bar.baz")
+			logging.get_logger("foo.bin")
+			logging.get_logger("foo")
+			logging.get_logger("foo.bin.bon")
 
-        it("child-parent-relationship", function()
-            logging.get_logger("foo.bar.baz")
-            logging.get_logger("foo.bin")
-            logging.get_logger("foo")
-            logging.get_logger("foo.bin.bon")
+			expect(logging.get_logger("foo").parent).to.equal("root")
+			expect(logging.get_logger("foo.bin").parent).to.equal("foo")
+			expect(logging.get_logger("foo.bin.bon").parent).to.equal("foo.bin")
+			expect(logging.get_logger("foo.bar.baz").parent).to.equal("foo")
+		end)
+	end)
 
-            expect(logging.get_logger("foo").parent).to.equal("root")
-            expect(logging.get_logger("foo.bin").parent).to.equal("foo")
-            expect(logging.get_logger("foo.bin.bon").parent).to.equal("foo.bin")
-            expect(logging.get_logger("foo.bar.baz").parent).to.equal("foo")
-        end)
-    end)
+	describe("handlers", function()
+		it("adhers-to-hierarchy", function()
+			local handler = {
+				name = "handle-test",
+				handle = function() end,
+			}
+			local spy = lust.spy(handler, "handle")
+			logging.get_logger("foo.bar"):add_handler(handler)
 
-    describe("handlers", function()
-        it("adhers-to-hierarchy", function()
-            local handler = {
-                name="handle-test",
-                handle=function() end
-            }
-            local spy = lust.spy(handler, 'handle')
-            logging.get_logger("foo.bar"):add_handler(handler)
+			logging.get_logger("foo").debug("Hi from foo")
+			logging.get_logger("foo.bar").debug("Hi from foo.bar")
+			logging.get_logger("foo.bar.baz").debug("Hi from foo.bar.baz")
 
-            logging.get_logger("foo").debug("Hi from foo")
-            logging.get_logger("foo.bar").debug("Hi from foo.bar")
-            logging.get_logger("foo.bar.baz").debug("Hi from foo.bar.baz")
-
-            expect(#spy).to.equal(2)
-        end)
-    end)
+			expect(#spy).to.equal(2)
+		end)
+	end)
 end)
-
-
